22dbdbb4255973bc053dbf2c314384d340cda67a
OKravtsiv <kravtsivostap@gmail.com>
2018-10-17 15:56:02 +0300

    Quick fix

--- src/test/java/com/softserveinc/testapi/restassured/UserControllerTest.java
+++ src/test/java/com/softserveinc/testapi/restassured/UserControllerTest.java
@@ -4,5 +4,5 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-public class UserControllerTest{
+public class UserControllerTest extends BaseTest{
 }
